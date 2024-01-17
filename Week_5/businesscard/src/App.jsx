38a08bab7128d91b5dec43b3ceec252a2fb3124c
Vanshil Patel <vanshilpatel12@gmail.com>
2024-01-17 10:44:17 +0530

    Level-1 completed

---
+++ Week_5/businesscard/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BusinessCard from './BusinessCard'
+
+const App = () => {
+  return (
+    <div>
+  <BusinessCard name="Vanshil" interests={['Tennis', 'Coding', 'Music']} description="Full stack dev" />
+
+    </div>
+  )
+}
+
+export default App
---
+++ Week_5/businesscard/src/BusinessCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './BusinessCard.css';  
+
+const BusinessCard = (props) => {
+  return (
+    <div className="business-card">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+
+     
+      <div className="interests-container">
+        <h2>Interests:</h2>
+        <ul className="interests-list">
+          {props.interests.map((interest, index) => (
+            <li key={index}>{interest}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="button-container">
+        <button type='button' className="social-button"><a href='/'>Twitter</a></button>
+        <button type='button' className="social-button"><a href='/'>LinkedIn</a></button>
+      </div>
+    </div>
+  );
+};
+
+export default BusinessCard;
